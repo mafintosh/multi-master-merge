1dd70a549b8e5492cba604bf4c39170d8f50c0cd
Ivo Georgiev <ivo@linvo.org>
2015-11-10 17:53:15 +0200

    implement opts.gc

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ var create = function (db, opts) {
         }, function (err) {
           if (err) return cb(err)
           seqs.put(data.peer, '' + data.seq, function () {
+            if (opts.gc) {
+              heads.forEach(function (key) {
+                var parts = split(key)
+                log.del(parts[0], parts[1])
+              })
+            }
+
             postupdate(d, function (err) {
               if (data.peer === log.id) call(head = data.seq)
               cb(err)
